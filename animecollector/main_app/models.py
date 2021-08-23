5cc71a74f7fd63f63c2a204e2d8adab9e9f98ebf
Charles Ata <charlesata@hotmail.ca>
2021-08-23 13:08:39 -0400

    Complete Pt.2 of Anime Collector Lab

--- animecollector/main_app/models.py
+++ animecollector/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+class Anime(models.Model):
+    title = models.CharField(max_length=100)
+    release_year = models.IntegerField()
+    description = models.TextField(max_length=400)
+    rating = models.PositiveIntegerField(validators=[MaxValueValidator(10)])
+
+    def __str__(self):
+        return self.title
